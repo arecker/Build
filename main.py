7ee34b6ea1d7c9276c493b22847fb408c9cc257c
Alex Recker <alex@reckerfamily.com>
2014-10-19 16:55:19 +0000

    This is working now

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 import json
 import subprocess
+import os
 
 app = Flask(__name__)
+app.debug=True
+pwd = os.path.dirname(os.path.realpath(__file__))
 
 # Open Config
-config_file = open("repos.json")
+config_file = open(os.path.join(pwd, "repos.json"))
 config = json.load(config_file)
 config_file.close()
 
@@ -16,14 +19,17 @@ def hello_world():
 
 
 @app.route("/", methods=['POST'])
-def hook_endpoint(request, user, repo):
+def hook_endpoint():
 	blob = request.get_json()
 	if blob["ref"] != "refs/heads/master":
 		return "No a change to master branch.  Exiting."
 	repo = blob["repository"]["full_name"]
 	target = config[repo]
-	subprocess.call([target])
-
+	exit_code = subprocess.call([target])
+        if exit_code is 0:
+            return "OK"
+        else:
+            return "Oops: " + str(exit_code)
 
 if __name__ == '__main__':
 	app.run(debug=True)
