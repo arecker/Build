c79f0a16f2f13c32ec4e6e7be94043f5c361d0a4
Alex Recker <alex@reckerfamily.com>
2014-10-19 10:42:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import json
+import subprocess
+
+app = Flask(__name__)
+
+# Open Config
+config_file = open("repos.json")
+config = json.load(config_file)
+config_file.close()
+
+
+@app.route("/", methods=['POST'])
+def hook_endpoint(request, user, repo):
+	blob = request.get_json()
+	if blob["ref"] != "refs/heads/master":
+		return "No a change to master branch.  Exiting."
+	repo = blob["repository"]["full_name"]
+	target = config[repo]
+	subprocess.call([target])
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
